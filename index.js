a38344417ebe7ec4c4a0672d5b0c7d6f9f2338d9
Pau Garcia <37461446+paugarcia32@users.noreply.github.com>
2022-12-27 22:12:32 +0100

    insignificant modifications

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 setInterval(function() {
     // Obtener la hora actual
     var date = new Date();
@@ -11,8 +12,25 @@ setInterval(function() {
     // Actualizar el elemento HTML con la hora formateada
     document.getElementById('clock').innerHTML = formattedTime;
   }, 200); // Actualizar cada 1000 milisegundos (cada segundo)
+*/
+
+
+
+// Obtener una referencia al elemento p con id "hora"
+const horaElement = document.getElementById("clock");
+
+// Actualizar el contenido del elemento p con la hora actual cada segundo
+setInterval(function() {
+  // Obtener la hora actual
+  const horaActual = new Date();
+
+  // Formatear la hora con dos dígitos y mostrarla en el elemento p
+  horaElement.textContent = horaActual.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+}, 200);
 
   
+
+
 
   
   fetch('https://api.openweathermap.org/data/2.5/weather?q=Barcelona,España&appid=dba306998ebe631bfd796148d0ff7a9b&units=metric')
@@ -22,6 +40,9 @@ setInterval(function() {
   .then(function(data) {
     // Obtener la temperatura en grados Celsius
     var temperature = data.main.temp;
+
+    // Redondear la temperatura a un número entero
+    temperature = Math.round(temperature);
 
     // Actualizar el elemento HTML con la temperatura
     document.getElementById('weather').innerHTML = temperature + ' ºC';
